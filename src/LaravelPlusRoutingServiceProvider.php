a830cea13836918691f50d8e5ae69e07385f30ca
Wu Weixin <wuweixin@gmail.com>
2017-07-05 17:45:09 +0800

    ServiceProvider file

---
+++ src/LaravelPlusRoutingServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Wuwx\LaravelPlusRouting;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPlusRoutingServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
